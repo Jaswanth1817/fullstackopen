2bd5d4adcc3ebf62bb67f1698fb95c1246b87b83
Jaswanth1817 <74001136+Jaswanth1817@users.noreply.github.com>
2021-05-29 20:24:04 +0530

    Add files via upload

--- Part1/course_contents/src/App.js
+++ Part1/course_contents/src/App.js
@@ -1,32 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'
-import DisplayCountryDetails from './components/DisplayCountryDetails'
+import React from 'react'
+import Course from './components/Course'
 
 const App = () => {
-  const [input, setInput] = useState('')
-  const [countries, setCountries] = useState([])
-  const [searchCountries, setSearchCountries] = useState([])
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
 
-  const hook = () => {
-    axios.get("https://restcountries.eu/rest/v2/all").then((response) => setCountries(response.data))
-  }
-  useEffect(hook, [])
+    return (
+        <div>
+            <h1>Web development curriculum</h1>
+            {courses.map(course =>
+                <Course key={course.id} course={course} />
+            )}
+        </div>
 
-  const findCounties = (event) => {
-    setInput(event.target.value)
-    const searchCountries = countries.filter((country) => country.name.toLowerCase().includes(event.target.value.toLowerCase()) === true)
-    setSearchCountries(searchCountries)
-  }
+    )
 
-  return (
-    <div>
-      <form>
-        find countries <input value={input} onChange={findCounties} />
-      </form>
-      <DisplayCountryDetails searchCountries={searchCountries} input={input} />
-    </div>
-  )
 }
-
 
 export default App
